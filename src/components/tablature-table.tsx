b406259e51906c0fab7a080160fadcc8f99c520f
meuzi8122 <kagiakete1234@gmail.com>
2025-02-16 00:14:41 +0900

    演奏動画有無の絞り込み

--- src/components/tablature-table.tsx
+++ src/components/tablature-table.tsx
@@ -1,13 +1,28 @@
+"use client";
+
 import { Tablature } from "@/types/domain";
 import Link from "next/link";
+import { ChangeEvent, useState } from "react";
 
 type Props = {
     tablatures: Tablature[];
 };
 
 export default function TablatureTable({ tablatures }: Props) {
+    const [hasVideo, setHasVideo] = useState<boolean>(false);
+
+    const handleHasVideoChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setHasVideo(event.currentTarget.checked);
+    };
+
     return (
         <div className="overflow-x-auto">
+            <div className="form-control mb-4">
+                <label className="label cursor-pointer">
+                    <span className="label-text">演奏動画があるTAB譜のみ表示</span>
+                    <input type="checkbox" className="toggle" checked={hasVideo} onChange={handleHasVideoChange} />
+                </label>
+            </div>
             <table className="table">
                 <thead>
                     <tr>
@@ -17,7 +32,8 @@ export default function TablatureTable({ tablatures }: Props) {
                     </tr>
                 </thead>
                 <tbody>
-                    {tablatures.map((tablature, index) => (
+                    {/* TODO: flatMapを使うべき？ */}
+                    {(hasVideo ? tablatures.filter((tablature) => tablature.hasVideo) : tablatures).map((tablature) => (
                         <tr key={tablature.id}>
                             <td>
                                 <a href={tablature.tablatureLink} target="_blank">
