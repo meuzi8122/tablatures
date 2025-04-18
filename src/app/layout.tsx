d51a54995b1330f0789a46ba81c096e6d6da412f
meuzi8122 <kagiakete1234@gmail.com>
2025-04-19 00:26:22 +0900

    move css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import DrawerSide from "@/component/drawer-side";
 import Navbar from "@/component/navbar";
 import { SITE_DESCRIPTION, SITE_TITLE } from "@/constant/site";
-import "@/style/globals.css";
+import "@/globals.css";
 import type { LayoutProps } from "@/type/layout";
 import type { Metadata } from "next/types";
 
