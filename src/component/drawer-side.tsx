3a5597d1c6edcf2f8904d47c0010efc465219a12
meuzi8122 <kagiakete1234@gmail.com>
2025-04-19 01:31:48 +0900

    next-auth導入

--- src/component/drawer-side.tsx
+++ src/component/drawer-side.tsx
@@ -1,28 +1,43 @@
-"use client";
+import { signInAction, signOutAction } from "@/action/user";
+import { auth } from "@/auth";
 
-import { useRouter } from "next/navigation";
+export default async function DrawerSide() {
+    const session = await auth();
 
-export default function DrawerSide() {
-    const router = useRouter();
+    // const router = useRouter();
 
     const handleAddTablatureButtonClick = async () => {
-        const res = await fetch("/api/tablatures", { method: "POST" });
-        if (res.status == 201) {
-            const { id } = await res.json();
-            router.push(`/tablatures/${id}`);
-        } else {
-            alert("新規作成に失敗しました。");
-        }
+        // const res = await fetch("/api/tablatures", { method: "POST" });
+        // if (res.status == 201) {
+        //     const { id } = await res.json();
+        //     router.push(`/tablatures/${id}`);
+        // } else {
+        //     alert("新規作成に失敗しました。");
+        // }
     };
 
     return (
         <div className="drawer-side">
             <label htmlFor="drawer" className="drawer-overlay"></label>
             <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+                {session === null ? (
+                    <li>
+                        <form action={signInAction}>
+                            <button type="submit">ログイン</button>
+                        </form>
+                    </li>
+                ) : (
+                    <li>
+                        <form action={signOutAction}>
+                            <button type="submit">ログアウト</button>
+                        </form>
+                    </li>
+                )}
                 <li>
-                    <a href="#" onClick={handleAddTablatureButtonClick}>
-                        TAB譜を追加
-                    </a>
+                    <a href="#">TAB譜を追加</a>
+                </li>
+                <li>
+                    <a>お気に入りのTAB譜</a>
                 </li>
                 <li>
                     <a>このサイトについて</a>
